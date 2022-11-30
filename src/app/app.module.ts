ee334f53aa5bcc87893a106071c9f7f60ab236ab
delmashajan <delmashajan@gmail.com>
2022-11-30 15:08:00 +0530

    about us added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    GalleryComponent
+    GalleryComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
