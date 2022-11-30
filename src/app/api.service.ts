db3014a2dc2e76f9656c258d630050014eb71cc9
delmashajan <delmashajan@gmail.com>
2022-11-30 17:03:40 +0530

    service added

---
+++ src/app/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  fetchFriend=()=>
+  {
+    return this.http.get("https://dummyapifriends.herokuapp.com/view")
+  }
+  fetchCourses=()=>
+  {
+    return this.http.get("https://mylinkurcodesapp.herokuapp.com/getcourses")
+  }
+}
